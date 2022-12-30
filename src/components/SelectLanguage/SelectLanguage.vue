f23856a65c921eb27992247b0f6837d6fd48985d
Vlad Ageysha <ander123fakk@gmail.com>
2022-12-30 17:30:21 +0300

    Add switching between languages

--- src/components/SelectLanguage/SelectLanguage.vue
+++ src/components/SelectLanguage/SelectLanguage.vue
@@ -1,24 +1,35 @@
 <script setup>
+    import { ref } from 'vue';
 
+    const lang = ref('EN')
+    const langIcon = ref('/src/assets/us.png')
+    const languages = ref([
+        {id: 1, name: 'EN', icon: '/src/assets/us.png'},
+        {id: 2, name: 'RU', icon: '/src/assets/russia.png'},
+    ])
+
+    function selectLang(id) {
+        lang.value = languages.value.find(lang => lang.id === id).name
+        langIcon.value = languages.value.find(lang => lang.id === id).icon
+    }
 </script>
     
 <template>
     <div class="header__select-lang">
-        <span class="select-lang__title">EN</span>
-        <img class="select-lang__flag-icon" src="@/assets/us.png">
+
+        <span class="select-lang__title">{{ lang }}</span>
+        <img class="select-lang__flag-icon" :src="langIcon">
         <img class="select-lang__arrow-icon" src="@/assets/arrow.png">
 
         <ul class="select-lang__lang-list">
             <div class="lang-list__arrow"></div>
-            <li class="lang-list__item">
-                <span class="lang-list__title">EN</span>
-                <img class="select-lang__flag-icon" src="@/assets/us.png">
-            </li>
-            <li class="lang-list__item">
-                <span class="lang-list__title">RU</span>
-                <img class="select-lang__flag-icon" src="@/assets/russia.png">
+
+            <li v-for="lang in languages" :id="lang.id" class="lang-list__item" @click="selectLang(lang.id)">
+                <span class="lang-list__title">{{ lang.name }}</span>
+                <img class="select-lang__flag-icon" :src="lang.icon">
             </li>
         </ul>
+
     </div>
 </template>
 
