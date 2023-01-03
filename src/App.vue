29ffdecaea8d55b4f907eb1e7eb4bd3e7b0f2b3e
Vlad Ageysha <ander123fakk@gmail.com>
2023-01-03 10:22:04 +0300

    Add base blocks for steps how-it-works

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import Header from '@/components/Header/Header.vue'
   import Preview from '@/components/Preview/Preview.vue'
+  import HowItWorks from './components/HowItWorks/HowItWorks.vue';
 </script>
 
 <template>
   <Header />
   <Preview />
+  <HowItWorks />
 </template>
 
 <style scoped>
--- src/components/HowItWorks/HowItWorks.vue
+++ src/components/HowItWorks/HowItWorks.vue
@@ -1,6 +1,55 @@
 <template>
     <div class="how-it-works">
-        
+        <div class="container">
+
+            <h1 class="how-it-works__header-title">Как это работает?</h1>
+
+            <ul class="how-it-works__list-steps">
+
+                <div class="list-steps__container">
+
+                    <li class="list-steps__item-steps">
+                        <div class="item-steps__ellipse">
+                            
+                        </div>
+                        <div class="item-steps__text">
+                            <h2 class="item-steps__title">Человек<br>делает селфи</h2>
+                            <p class="item-steps__subtitle">Вы делаете селфи<br>или выбираете удачное фото<br>из галереи JoyKa</p>
+                        </div>
+                    </li>
+                    
+                    <li class="list-steps__item-steps">
+                        <div class="item-steps__ellipse">
+                            <div class="item-steps__ellipse-overlay ellipse-overlay-half"></div>
+                        </div>
+                        <div class="item-steps__text">
+                            <h2 class="item-steps__title">Конверт<br>JoyKa с ярким фото</h2>
+                            <p class="item-steps__subtitle">Оставляете заявку на сайте<br>или в приложении. Две минуты<br> — и JoyKa готова!</p>
+                        </div>
+                    </li>
+
+                    <li class="list-steps__item-steps">
+                        <div class="item-steps__ellipse">
+                            <div class="item-steps__ellipse-overlay"></div>
+                        </div>
+                        <div class="item-steps__text">
+                            <h2 class="item-steps__title">Курьер несёт<br>JoyKa</h2>
+                            <p class="item-steps__subtitle">Распечатанную фотографию<br>в красивом конверте курьер<br>доставит лично в руки адресату</p>
+                        </div>
+                    </li>
+
+                </div>
+
+                <li class="list-steps__item-steps">
+                    <div class="item-steps__ellipse"></div>
+                    <div class="item-steps__text">
+                        <h2 class="item-steps__title"></h2>
+                        <p class="item-steps__subtitle"></p>
+                    </div>
+                </li>
+            </ul>
+
+        </div>
     </div>
 </template>
 
