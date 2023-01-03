b451d431ff1581e1cefaea3327ccba26f094c006
Vlad Ageysha <ander123fakk@gmail.com>
2023-01-03 11:37:12 +0300

    Add img for steps blocks

--- src/components/HowItWorks/HowItWorks.vue
+++ src/components/HowItWorks/HowItWorks.vue
@@ -9,8 +9,8 @@
                 <div class="list-steps__container">
 
                     <li class="list-steps__item-steps">
-                        <div class="item-steps__ellipse">
-                            
+                        <div class="item-steps__wrap-img">
+                            <img class="item-steps__img" src="@/assets/girl-selfie.png">
                         </div>
                         <div class="item-steps__text">
                             <h2 class="item-steps__title">Человек<br>делает селфи</h2>
@@ -19,8 +19,8 @@
                     </li>
                     
                     <li class="list-steps__item-steps">
-                        <div class="item-steps__ellipse">
-                            <div class="item-steps__ellipse-overlay ellipse-overlay-half"></div>
+                        <div class="item-steps__wrap-img">
+                            <img class="item-steps__img" src="@/assets/girl-phone.png">
                         </div>
                         <div class="item-steps__text">
                             <h2 class="item-steps__title">Конверт<br>JoyKa с ярким фото</h2>
@@ -29,8 +29,8 @@
                     </li>
 
                     <li class="list-steps__item-steps">
-                        <div class="item-steps__ellipse">
-                            <div class="item-steps__ellipse-overlay"></div>
+                        <div class="item-steps__wrap-img">
+                            <img class="item-steps__img" src="@/assets/man-chocolate.png">
                         </div>
                         <div class="item-steps__text">
                             <h2 class="item-steps__title">Курьер несёт<br>JoyKa</h2>
@@ -43,8 +43,8 @@
                 <li class="list-steps__item-steps">
                     <div class="item-steps__ellipse"></div>
                     <div class="item-steps__text">
-                        <h2 class="item-steps__title"></h2>
-                        <p class="item-steps__subtitle"></p>
+                        <h2 class="item-steps__title">Адресат радуется полученной JoyKa</h2>
+                        <p class="item-steps__subtitle">Ваше фото с личным посланием подарят близким хорошее настроение на весь день</p>
                     </div>
                 </li>
             </ul>
