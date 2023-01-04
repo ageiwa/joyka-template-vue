1954c270b34d7426eb82f780ce8129ab1d927916
Vlad Ageysha <ander123fakk@gmail.com>
2023-01-04 11:30:49 +0300

    Add ReasonForSend component and background for him

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
   import Header from '@/components/Header/Header.vue'
   import Preview from '@/components/Preview/Preview.vue'
-  import HowItWorks from './components/HowItWorks/HowItWorks.vue';
+  import HowItWorks from '@/components/HowItWorks/HowItWorks.vue'
+  import ReasonForSend from '@/components/ReasonForSend/ReasonForSend.vue'
 </script>
 
 <template>
   <Header />
   <Preview />
   <HowItWorks />
+  <ReasonForSend />
 </template>
 
 <style scoped>
---
+++ src/components/ReasonForSend/ReasonForSend.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="reason-for-send">
+        <div class="reason-for-send__clouds"></div>
+
+        <div class="container">
+            <h1 class="reason-for-send__header-title">Поводы для отправки</h1>
+
+
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    @import './style.scss'
+</style>
