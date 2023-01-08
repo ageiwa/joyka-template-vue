7f8bdac1009a018a14d712cd3bcf622ea5d71bfb
Vlad Ageysha <ander123fakk@gmail.com>
2023-01-08 12:21:26 +0300

    Made mobile menu

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -2,6 +2,13 @@
   import SelectLanguage from '@/components/SelectLanguage/SelectLanguage.vue'
   import './style.scss'
   import './media.scss'
+  import { ref } from 'vue';
+
+  const showMobileMenu = ref(false)
+
+  function openMenu() {
+    showMobileMenu.value = !showMobileMenu.value
+  }
 </script>
 
 <template>
@@ -26,12 +33,15 @@
 
       <SelectLanguage />
 
-      <div class="burger-menu">
-        <div class="burger-menu__line"></div>
-        <div class="burger-menu__line"></div>
-        <div class="burger-menu__line"></div>
+      <div class="burger-menu-wrap">
+        <div @click="openMenu" :class="{'burger-menu': true, 'burger-menu_active': showMobileMenu}">
+          <div class="burger-menu__line"></div>
+          <div class="burger-menu__line"></div>
+          <div class="burger-menu__line"></div>
+        </div>
+      </div>
 
-        <!-- <nav class="header__navbar-mobile">
+      <nav :class="{'header__navbar-mobile': true, 'header__navbar-mobile_active': showMobileMenu}">
           <ul>
             <li class="navbar-mobile__item"><a href="#">Как это работает?</a></li>
             <li class="navbar-mobile__item"><a href="#">Кто мы?</a></li>
@@ -39,8 +49,7 @@
             <li class="navbar-mobile__item"><a href="#">FAQ</a></li>
             <li class="navbar-mobile__item"><a href="#">Отзывы</a></li>
           </ul>
-        </nav> -->
-      </div>
+        </nav>
 
     </div>
   </header>
